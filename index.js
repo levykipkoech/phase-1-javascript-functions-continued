366866cbd6c6ca5cbfc4313e8c6950d33792b6a7
levy kipkoech <kipkoechlevy096@gmail.com>
2022-11-28 08:45:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+//    return ("This Saturday, I want to roller-skate!");
+  return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun ("bathe my dog");
+
+function mondayWork (task='go to the office') {
+    // return ("This Monday, I will go to yhe office")
+    return (`This Monday, I will ${task}.`)
+}
+ mondayWork("work from home")
+
+  function wrapAdjective(style='*'){
+     return function (condition='special'){
+         return `You are ${style}${condition}${style}!`
+     }
+    
+  }
+  
